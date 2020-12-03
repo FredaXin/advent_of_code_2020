f9f5b82d7288514d2a6aa8498dac9c674d2ac589
Freda Xin <freda.xin@gmail.com>
2020-12-03 16:07:14 -0500

    day2 solution

---
+++ day3/day3.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+
+header_list = ['temp']
+df = pd.read_csv('input.txt', names=header_list)
+test = pd.read_csv('test.txt', names=header_list)
+
+
+# Part 1 
+
+# Why 100? Welp I think it's long enough, lol.
+df['temp'] = [100*i for i in df['temp']]
+
+def check_tree(dataframe, down, right): 
+    tree_sum = 0 
+    position = 0
+    num_of_row = dataframe.shape[0]
+
+    for i in range(0, num_of_row, down):
+        if dataframe.loc[i]['temp'][position] == '#':
+            tree_sum += 1
+        position += right
+
+    return tree_sum
+    
+print(check_tree(df, 1, 3))
+
+
+# Part 2
+def all_slopes(tup_list, dataframe):
+    
+    dataframe['temp'] = [100*i for i in dataframe['temp']]
+    sum_list = []
+
+    for (right, down) in tup_list:
+        each_slope = check_tree(dataframe, down, right)
+        sum_list.append(each_slope)
+    return np.prod(sum_list)
+
+tup_list = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+print(all_slopes(tup_list, df))
+
+
+
+
+
